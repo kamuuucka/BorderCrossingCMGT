ce4ba7030099d81edc8223a0568d60544ee3e2c2
Kamila <kamila.matuszak@list.pl>
2024-11-18 11:11:56 +0100

    Graph slicing properly

--- BorderCrossing/Assets/Scripts/Spline/SplineGraphGenerator.cs
+++ BorderCrossing/Assets/Scripts/Spline/SplineGraphGenerator.cs
@@ -8,30 +8,29 @@ using UnityEngine.Splines;
 public class SplineGraphGenerator : MonoBehaviour
 {
     #region Exposed Variables
-    [Header("Graph Specifications")]
-    [Tooltip("Width of the layers in the graph.")]
-    [Range(3,10)]
-    [SerializeField] private float layerWidth;
-    
-    [Tooltip("Starting radius if the circle.")]
-    [Range(2,5)]
-    [SerializeField] private float radius;
-    
-    [Tooltip("Materials for each layer in the graph. First colour will be the one in the middle of the graph.")]
-    [SerializeField] private List<Material> graphMaterials;
 
-    [Space(10)] [Header("Necessary Objects")] 
-    [Tooltip("Data with the prompts.")] 
-    [SerializeField] private StringData prompts;
+    [Header("Graph Specifications")] [Tooltip("Width of the layers in the graph.")] [Range(3, 10)] [SerializeField]
+    private float layerWidth;
+
+    [Tooltip("Starting radius if the circle.")] [Range(2, 5)] [SerializeField]
+    private float radius;
+
+    [Tooltip("Materials for each layer in the graph. First colour will be the one in the middle of the graph.")]
+    [SerializeField]
+    private List<Material> graphMaterials;
+
+    [Space(10)] [Header("Necessary Objects")] [Tooltip("Data with the prompts.")] [SerializeField]
+    private StringData prompts;
+
     #endregion
 
     #region Private Variables
 
     private readonly float _smoothing = 1.64f;
-    private List<ChunkWithLayers> _chunksWithLayers = new();
+    private List<SegmentWithLayers> _chunksWithLayers = new();
 
     #endregion
-    
+
 
     private void Start()
     {
@@ -47,63 +46,65 @@ public class SplineGraphGenerator : MonoBehaviour
             return;
         }
         
-        var layers = graphMaterials.Count;
-        // for (int i = 0; i < layers; i++)
-        // {
-        //     var newObj = new GameObject($"Layer{i + 1}");
-        //     newObj.transform.SetParent(transform);
-        //     var splineGenerator = newObj.AddComponent<SplitSplineGenerator>();
-        //     var circularSpline = splineGenerator.CreateCircularSpline(radius + (layerWidth + layerWidth) * i);
-        //     var splitSplines = splineGenerator.SplitSpline(circularSpline, prompts.data.Count, _smoothing);
-        //
-        //     foreach (var spline in splitSplines)
-        //     {
-        //         splineGenerator.ExtrudeSpline(spline, graphMaterials[i], layerWidth);
-        //     }
-        // }
+        CreateSegments(prompts.data.Count);
+        SpawnGraphOnChunks(graphMaterials.Count, prompts.data.Count);
+    }
 
-        for (int i = 0; i < prompts.data.Count; i++)
+    /// <summary>
+    /// Create a graph divided into layers and segments
+    /// </summary>
+    /// <param name="numberOfLayers">Number of layers the graph should have</param>
+    /// <param name="numberOfChunks">Number of segments the graph should be divided into</param>
+    private void SpawnGraphOnChunks(int numberOfLayers, int numberOfChunks)
+    {
+        for (var i = 0; i < numberOfLayers; i++)
+        {
+            //Create temp object just to create SplitSplineGenerator
+            var tempObject = new GameObject();
+            var splineGenerator = tempObject.AddComponent<SplitSplineGenerator>();
+            var circularSpline = splineGenerator.CreateCircularSpline(radius + (layerWidth + layerWidth) * i);
+            var splitSplines = splineGenerator.SplitSpline(circularSpline, prompts.data.Count, _smoothing);
+            
+            for (int j = 0; j < numberOfChunks; j++)
+            {
+                _chunksWithLayers[j].segments.Add(splitSplines[j]);
+                _chunksWithLayers[j].segments[i].transform.SetParent(_chunksWithLayers[j].chunk.transform);
+            }
+            
+            foreach (var spline in splitSplines)
+            {
+                splineGenerator.ExtrudeSpline(spline, graphMaterials[i], layerWidth);
+            }
+            
+            Destroy(tempObject);
+        }
+    }
+
+    /// <summary>
+    /// Create segments with lists of layers that can be added to them
+    /// </summary>
+    /// <param name="numberOfChunks">Number of segments the graph should be divided into</param>
+    private void CreateSegments(int numberOfChunks)
+    {
+        for (int i = 0; i < numberOfChunks; i++)
         {
             var chunkParent = new GameObject($"Chunk{i + 1}")
             {
-                transform = { parent = transform}
+                transform = { parent = transform }
             };
 
-            var chunkWithLayers = new ChunkWithLayers
+            var chunkWithLayers = new SegmentWithLayers
             {
                 chunk = chunkParent,
                 segments = new List<SplineContainer>()
             };
             _chunksWithLayers.Add(chunkWithLayers);
         }
-
-        //TODO: Fix this because it is not dividing to chunks correctly:(
-        for (int i = 0; i < layers; i++)
-        {
-            var newObj = new GameObject();
-            var splineGenerator = newObj.AddComponent<SplitSplineGenerator>();
-            var circularSpline = splineGenerator.CreateCircularSpline(radius + (layerWidth + layerWidth) * i);
-            
-            for (int j = 0; j < prompts.data.Count; j++)
-            {
-                newObj.transform.SetParent(_chunksWithLayers[j].chunk.transform);
-                
-                var splitSplines = splineGenerator.SplitSpline(circularSpline, prompts.data.Count, _smoothing);
-
-                foreach (var spline in splitSplines)
-                {
-                    splineGenerator.ExtrudeSpline(spline, graphMaterials[i], layerWidth);
-                }
-                
-                _chunksWithLayers[j].segments = splitSplines;
-            }
-        }
     }
-    
 }
 
 [Serializable]
-public class ChunkWithLayers
+public class SegmentWithLayers
 {
     public GameObject chunk;
     public List<SplineContainer> segments;
