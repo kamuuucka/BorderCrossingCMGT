78453c5422f479e87028bf2f858e7ca7f24c29bc
Kamila <kamila.matuszak@list.pl>
2024-11-25 15:37:52 +0100

    Data saved in two different files

--- BorderCrossing/Assets/Scripts/DataHandling/DataPersistenceManager.cs
+++ BorderCrossing/Assets/Scripts/DataHandling/DataPersistenceManager.cs
@@ -8,15 +8,19 @@ using UnityEngine.Serialization;
 public class DataPersistenceManager : MonoBehaviour
 {
     [SerializeField] private string fileName;
+    [SerializeField] private string discussionFileName;
     [SerializeField] private StringData promptsToUse;
     [SerializeField] private StringData defaultPrompts;
+    [SerializeField] private StringData discussionToUse;
     [SerializeField] private SettingsManager settingsManager;
     [SerializeField] private bool isDebug;
 
     public static DataPersistenceManager Instance { get; private set; }
     private PromptsData _promptsData;
+    private PromptsData _discussionsData;
     private PromptsData.Prompts _activePrompt;
     private FileDataHandler _questionsDataHandler;
+    private FileDataHandler _discussionsDataHandler;
     private List<IDataPersistence> _dataPersistenceObjects;
 
     private void Awake()
@@ -32,6 +36,7 @@ public class DataPersistenceManager : MonoBehaviour
     private void Start()
     {
         _questionsDataHandler = new FileDataHandler(Application.persistentDataPath, fileName);
+        _discussionsDataHandler = new FileDataHandler(Application.persistentDataPath, discussionFileName);
         _dataPersistenceObjects = FindAllDataPersistenceObjects();
         LoadGame();
     }
@@ -44,42 +49,72 @@ public class DataPersistenceManager : MonoBehaviour
         _promptsData.AddNewPrompts("General questions about transgressive behaviour", defaultPrompts.data);
         _promptsData.promptList[0].basePrompt = true;
         _promptsData.promptList[0].active = true;
+        _discussionsData = new PromptsData();
+        _discussionsData.AddNewPrompts("afbhabfhabhfba", discussionToUse.data);
+        _discussionsData.promptList[0].basePrompt = true;
+        _discussionsData.promptList[0].active = true;
     }
 
     private void LoadGame()
     {
         _promptsData = _questionsDataHandler.Load();
+        _discussionsData = _discussionsDataHandler.Load();
 
-        if (_promptsData == null)
+        if (_promptsData == null || _discussionsData == null)
         {
-            if(isDebug) Debug.Log("There's no game data");
+            if(isDebug) Debug.Log("GameData missing!");
             NewGame();
         }
 
         foreach (var dataPersistenceObject in _dataPersistenceObjects)
         {
             dataPersistenceObject.LoadData(_promptsData);
+            dataPersistenceObject.LoadData(_discussionsData);
         }
 
-        var foundActive = false;
+        var foundActivePrompt = false;
         foreach (var prompt in _promptsData.promptList)
         {
             if(isDebug) Debug.Log($"Am I active? {prompt.active}");
-            if (prompt.active && !foundActive)
+            if (prompt.active && !foundActivePrompt)
             {
-                foundActive = true;
+                foundActivePrompt = true;
                 UseTheSave(_promptsData.promptList.IndexOf(prompt));
             }
-            else if (prompt.active && foundActive)
+            else if (prompt.active && foundActivePrompt)
             {
                 prompt.active = false;
             }
         }
 
-        if (foundActive) return;
+        if (!foundActivePrompt)
+        {
+            if(isDebug) Debug.Log("Setting the first prompt as active");
+            _promptsData.promptList[0].active = true;
+        }
         
-        if(isDebug) Debug.Log("Setting the first prompt as active");
-        _promptsData.promptList[0].active = true;
+        var foundActiveDiscussion = false;
+        foreach (var discussion in _discussionsData.promptList)
+        {
+            if(isDebug) Debug.Log($"Am I active? {discussion.active}");
+            if (discussion.active && !foundActiveDiscussion)
+            {
+                foundActiveDiscussion = true;
+                UseTheSave(_discussionsData.promptList.IndexOf(discussion));
+            }
+            else if (discussion.active && foundActiveDiscussion)
+            {
+                discussion.active = false;
+            }
+        }
+
+        if (!foundActiveDiscussion)
+        {
+            if(isDebug) Debug.Log("Setting the first discussion as active");
+            _discussionsData.promptList[0].active = true;
+        }
+        
+        
     }
 
     public void SaveGame()
@@ -87,9 +122,11 @@ public class DataPersistenceManager : MonoBehaviour
         foreach (var dataPersistenceObject in _dataPersistenceObjects)
         {
             dataPersistenceObject.SaveData(ref _promptsData);
+            dataPersistenceObject.SaveData(ref _discussionsData);
         }
 
         _questionsDataHandler.Save(_promptsData);
+        _discussionsDataHandler.Save(_discussionsData);
     }
 
     public void DeleteSave(int id)
