27018c3c2606dc9ca03cd531ec0fab85d0a347d9
Kamila <kamila.matuszak@list.pl>
2024-12-04 15:00:53 +0100

    Added the EditorManager script

---
+++ BorderCrossing/Assets/Scripts/EditorManager.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class EditorManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] objectsToDisable;
+
+    private void DisableScenes()
+    {
+        if (objectsToDisable.Length > 0)
+        {
+            foreach (var objectToDisable in objectsToDisable)
+            {
+                objectToDisable.SetActive(false);
+            }
+        }
+    }
+
+    private void OnValidate()
+    {
+        DisableScenes();
+    }
+}
--- BorderCrossing/Assets/Scripts/Tools/DisableOnStart.cs
+++ BorderCrossing/Assets/Scripts/Tools/DisableOnStart.cs
@@ -1,19 +1,14 @@
+using System;
 using UnityEngine;
 
 /// <summary>
-/// Disables the object or specific methods on start.
+/// Disables specific methods on start.
 /// </summary>
 public class DisableOnStart : MonoBehaviour
 {
-    [SerializeField] private bool wholeObject = true;
     [SerializeField] private MonoBehaviour[] scriptsToDisable;
     private void Awake()
     {
-        if (wholeObject)
-        {
-            gameObject.SetActive(false);
-        }
-
         foreach (var script in scriptsToDisable)
         {
             if (script != null)
